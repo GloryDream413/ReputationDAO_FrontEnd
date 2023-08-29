2ef860991d259459ecdd3a51fe7c9aa2d81fe0fa
GloryDream413 <glorydream413@gmail.com>
2023-08-29 16:43:45 +0900

    Remove unnecessary files.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header, Dream, Footer, Dashboard } from './components'
+import { Dashboard } from './components'
 import { useState, createContext } from 'react'
 import './App.css'
 import { ToastContainer } from 'react-toastify';
@@ -10,18 +10,11 @@ const App = () => {
   const [nftRoute, setNftRoute] = useState('')
   return (
     <UserContext.Provider value={{ nftRoute, setNftRoute }}>
-      <div>
-          <div className="gradient__bg">
-            {/* <Header /> */}
-            <Router>
-              <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/dream" element={<Dream />} />
-              </Routes>
-            </Router>
-          </div>
-          {/* <Footer/> */}
-      </div>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </Router>
       <ToastContainer autoClose={3000} draggableDirection='x' />
     </UserContext.Provider>
   );
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,34 +1,15 @@
 import React from 'react';
 import './dashboard.css';
-import aiorigin from '../../assets/aiorigin.png'
-import aigenerated from '../../assets/aigenerated.png'
-import { Link } from 'react-router-dom';
-import GalleryItem1 from '../galleryitem/Galleryitem1';
-import GalleryItem2 from '../galleryitem/Galleryitem2';
-import GalleryItem3 from '../galleryitem/Galleryitem3';
-import GalleryItem4 from '../galleryitem/Galleryitem4';
-import GalleryItem5 from '../galleryitem/Galleryitem5';
-import GalleryItem6 from '../galleryitem/Galleryitem6';
+import WalletConnectLogo from '../../assets/WalletConnectLogo.png'
 
 export const Dashboard = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
-
       <div className="gpt3__header-content">
         <div className="original">
-          {/* <img src={aiorigin} alt="ai" /> */}
+          <img src={WalletConnectLogo} alt="ai" />
         </div>
       </div>
-      {/* <div className="gpt3__header-content">
-        <div className='gallery'><GalleryItem1 /></div>
-        <div className='gallery'><GalleryItem2 /></div>
-        <div className='gallery'><GalleryItem3 /></div>
-      </div>
-      <div className="gpt3__header-content">
-        <div className='gallery'><GalleryItem4 /></div>
-        <div className='gallery'><GalleryItem5 /></div>
-        <div className='gallery'><GalleryItem6 /></div>
-      </div> */}
     </div>
   );
 };
