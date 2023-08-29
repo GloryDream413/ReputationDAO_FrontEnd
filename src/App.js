898c2b9571a13c932e832d8f0732620a83ec0a02
GloryDream413 <glorydream413@gmail.com>
2023-08-29 15:10:23 +0900

    FrontEnd updated.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header, Dream, Footer, Dashboard } from './components'
 import { useState, createContext } from 'react'
 import './App.css'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 export const UserContext = createContext(null)
 const App = () => {
