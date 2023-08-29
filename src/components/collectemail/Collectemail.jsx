c28242c167945ee51c510777c530f2dccfbc7717
GloryDream413 <glorydream413@gmail.com>
2023-08-30 08:16:48 +0900

    First page displaying function added.

--- src/components/collectemail/Collectemail.jsx
+++ src/components/collectemail/Collectemail.jsx
@@ -4,6 +4,9 @@ import WalletConnectLogo from '../../assets/WalletConnectLogo.png'
 import genesis_icon from '../../assets/genesis_icon.png'
 import airdrop_icon from '../../assets/airdrop_icon.png'
 import dao_icon from '../../assets/dao_icon.png'
+import vector_icon from '../../assets/vector_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import telegram_icon from '../../assets/telegram_icon.png'
 import { Link } from 'react-router-dom';
 
 export const Collectemail = () => {
@@ -22,6 +25,11 @@ export const Collectemail = () => {
         <div className='airdrop_icon'>
             <img src={airdrop_icon} alt="airdrop_icon"/>
             <h1>Airdrop</h1>
+        </div>
+        <div className='social_icon'>
+            <img src={vector_icon} alt="vector_icon"/>
+            <img src={twitter_icon} alt="twitter_icon"/>
+            <img src={telegram_icon} alt="telegram_icon"/>
         </div>
       </div>
       <div className='content'></div>
