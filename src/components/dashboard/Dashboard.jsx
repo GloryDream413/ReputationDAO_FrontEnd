d8f959cd7b0226de63b5b57594bfaa7079b29cd6
GloryDream413 <glorydream413@gmail.com>
2023-08-29 21:07:41 +0900

    Wallet Connect Interface added.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './dashboard.css';
 import WalletConnectLogo from '../../assets/WalletConnectLogo.png'
+import genesis_icon from '../../assets/genesis_icon.png'
+import airdrop_icon from '../../assets/airdrop_icon.png'
+import dao_icon from '../../assets/dao_icon.png'
 
 export const Dashboard = () => {
   return (
@@ -8,6 +11,11 @@ export const Dashboard = () => {
       <div className="walletconnect">
         <img src={WalletConnectLogo}/>
         <button><h1>Connect Wallet</h1></button>
+        <div className='icons'>
+          <img src={airdrop_icon}/>
+          <img src={dao_icon}/>
+          <img src={genesis_icon}/>
+        </div>
       </div>
     </div>
   );
