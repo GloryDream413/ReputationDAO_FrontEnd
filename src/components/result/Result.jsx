e63d9621359bf18d96c6e33d4877ea681e736345
GloryDream413 <glorydream413@gmail.com>
2023-09-06 15:15:36 +0900

    Result Page Responsive Design completed.

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useMedia } from 'react-use';
 import './result.css';
 import WalletConnectLogo from '../../assets/WalletConnectLogo.png'
 import mainlogo from '../../assets/mainlogo.png'
@@ -40,152 +41,314 @@ export const Result = () => {
     return () => clearInterval(interval);
   }, [seconds]);
 
+  const below600 = useMedia('(max-width: 600px)')
   return (
-    <div className="mainsection2">
-      <div className='menu'>
-        <div className='logoitem'>
-          <img src={WalletConnectLogo} alt="walletconnectlogo"/>
-          <div className='menuitems'>
-            <Link to="/collectemail">
-            <div className='genesis_icon'>
-                <img src={genesis_icon} alt="genesis_icon"/>
-                <h1>Genesis</h1>
-            </div>
-            </Link>
-            <div className='dao_icon'>
-                <img src={dao_icon} alt="dao_icon"/>
-                <h1>DAO</h1>
-            </div>
-            <div className='airdrop_icon'>
-                <img src={airdrop_icon} alt="airdrop_icon"/>
-                <h1>Airdrop</h1>
+    <div>
+      {!below600 && (
+      <div className="mainsection2">
+        <div className='menu'>
+          <div className='logoitem'>
+            <img src={WalletConnectLogo} alt="walletconnectlogo"/>
+            <div className='menuitems'>
+              <Link to="/collectemail">
+              <div className='genesis_icon'>
+                  <img src={genesis_icon} alt="genesis_icon"/>
+                  <h1>Genesis</h1>
+              </div>
+              </Link>
+              <div className='dao_icon'>
+                  <img src={dao_icon} alt="dao_icon"/>
+                  <h1>DAO</h1>
+              </div>
+              <div className='airdrop_icon'>
+                  <img src={airdrop_icon} alt="airdrop_icon"/>
+                  <h1>Airdrop</h1>
+              </div>
             </div>
           </div>
+          <div className='social_icon'>
+              <img src={vector_icon} alt="vector_icon"/>
+              <img src={twitter_icon} alt="twitter_icon"/>
+              <img src={telegram_icon} alt="telegram_icon"/>
+          </div>
         </div>
-        <div className='social_icon'>
-            <img src={vector_icon} alt="vector_icon"/>
-            <img src={twitter_icon} alt="twitter_icon"/>
-            <img src={telegram_icon} alt="telegram_icon"/>
+        <div className='content'>
+          <div className='real-content'>
+            <div className='walletinfo'>
+                <div className='walletaddress'>
+                    <img src={connected_status_icon} alt="connected_status_icon"/>
+                    <h1>{walletAddress.substring(0, 4) + "..." + walletAddress.substring(walletAddress.length-2, walletAddress.length)}</h1>
+                </div>
+            </div>
+            <div className='mainlogo'>
+                <img src={mainlogo} alt="mainlogo"/>
+            </div>
+            <div className='title'>
+                <h1>Results</h1>
+            </div>
+            <div className='description'>
+                <h1>Once votes have been collected, the DAO will be open for proposals and use these principles and criteria to evaluate proposals!</h1>
+            </div>
+            <div className='timelabel'>
+              <h1>Genesis ends in:</h1> 
+            </div>
+            <div className='timedialog'>
+              <div className='timedialogdisplay'>
+                <img src={clock_icon} alt="clock_icon"/>
+                <h1>{parseInt(seconds/(24*60*60))}D, {parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))}h, {parseInt((seconds - parseInt(seconds/(24*60*60))*24*60*60 - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))* 60*60)/60)}m, {seconds - parseInt(seconds/(24*60*60))*(24*60*60) - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))*(60*60) - parseInt((seconds - parseInt(seconds/(24*60*60))*24*60*60 - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))* 60*60)/60)*60}s</h1>
+              </div>
+            </div>
+            <div className='userselect'>
+                <div className='userselectboard'>
+                    <div className='first'>
+                        <h1>1.</h1>
+                        <label></label>
+                    </div>
+                    <div className='second'>
+                        <h1>2.</h1>
+                        <label></label>
+                    </div>
+                    <div className='third'>
+                        <h1>3.</h1>
+                        <label></label>
+                    </div>
+                    <div className='fourth'>
+                        <h1>4.</h1>
+                        <label></label>
+                    </div>
+                    <div className='fifth'>
+                        <h1>5.</h1>
+                        <label></label>
+                    </div>
+                </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Commercialise reputation and verification services.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+              <div className='option'>
+                <h1>Provide liquidity to other protocols.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Utilise treasury funds in yield and staking.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+              <div className='option'>
+                <h1>Fund bad actor detection and bounty hunting.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Fund development of RDAO ecosystem.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+              <div className='option'>
+                <h1>Actively manage DAO as an investment fund.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Invest in early stage protocols.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+              <div className='option'>
+                <h1>Invest in NFT’s.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='buttoncontent'>
+          </div>
         </div>
       </div>
-      <div className='content'>
-        <div className='real-content'>
-          <div className='walletinfo'>
-              <div className='walletaddress'>
-                  <img src={connected_status_icon} alt="connected_status_icon"/>
-                  <h1>{walletAddress.substring(0, 4) + "..." + walletAddress.substring(walletAddress.length-2, walletAddress.length)}</h1>
+      )}
+      {below600 && (
+      <div className="mainsection_mobile">
+        <div className='menu'>
+          <div className='logoitem'>
+            <img src={WalletConnectLogo} alt="walletconnectlogo"/>
+            <div className='menuitems'>
+              <Link to="/collectemail">
+              <div className='genesis_icon'>
+                  <img src={genesis_icon} alt="genesis_icon"/>
+                  <h1>Genesis</h1>
               </div>
-          </div>
-          <div className='mainlogo'>
-              <img src={mainlogo} alt="mainlogo"/>
-          </div>
-          <div className='title'>
-              <h1>Results</h1>
-          </div>
-          <div className='description'>
-              <h1>Once votes have been collected, the DAO will be open for proposals and use these principles and criteria to evaluate proposals!</h1>
-          </div>
-          <div className='timelabel'>
-            <h1>Genesis ends in:</h1> 
-          </div>
-          <div className='timedialog'>
-            <div className='timedialogdisplay'>
-              <img src={clock_icon} alt="clock_icon"/>
-              <h1>{parseInt(seconds/(24*60*60))}D, {parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))}h, {parseInt((seconds - parseInt(seconds/(24*60*60))*24*60*60 - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))* 60*60)/60)}m, {seconds - parseInt(seconds/(24*60*60))*(24*60*60) - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))*(60*60) - parseInt((seconds - parseInt(seconds/(24*60*60))*24*60*60 - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))* 60*60)/60)*60}s</h1>
-            </div>
-          </div>
-          <div className='userselect'>
-              <div className='userselectboard'>
-                  <div className='first'>
-                      <h1>1.</h1>
-                      <label></label>
-                  </div>
-                  <div className='second'>
-                      <h1>2.</h1>
-                      <label></label>
-                  </div>
-                  <div className='third'>
-                      <h1>3.</h1>
-                      <label></label>
-                  </div>
-                  <div className='fourth'>
-                      <h1>4.</h1>
-                      <label></label>
-                  </div>
-                  <div className='fifth'>
-                      <h1>5.</h1>
-                      <label></label>
-                  </div>
+              </Link>
+              <div className='dao_icon'>
+                  <img src={dao_icon} alt="dao_icon"/>
+                  <h1>DAO</h1>
               </div>
-          </div>
-          <div className='options'>
-            <div className='option'>
-              <h1>Commercialise reputation and verification services.</h1>
-              <div className='checkbuttons'>
-                <img src={confirm_check} alt="confirm_check"/>
-                <h1>75%</h1>
-              </div>
-            </div>
-            <div className='option'>
-              <h1>Provide liquidity to other protocols.</h1>
-              <div className='checkbuttons'>
-                <img src={confirm_check} alt="confirm_check"/>
-                <h1>75%</h1>
+              <div className='airdrop_icon'>
+                  <img src={airdrop_icon} alt="airdrop_icon"/>
+                  <h1>Airdrop</h1>
               </div>
             </div>
           </div>
-          <div className='options'>
-            <div className='option'>
-              <h1>Utilise treasury funds in yield and staking.</h1>
-              <div className='checkbuttons'>
-                <h1>55%</h1>
-                <img src={confirm_cross} alt="confirm_cross"/>
-              </div>
-            </div>
-            <div className='option'>
-              <h1>Fund bad actor detection and bounty hunting.</h1>
-              <div className='checkbuttons'>
-                <h1>55%</h1>
-                <img src={confirm_cross} alt="confirm_cross"/>
-              </div>
-            </div>
-          </div>
-          <div className='options'>
-            <div className='option'>
-              <h1>Fund development of RDAO ecosystem.</h1>
-              <div className='checkbuttons'>
-                <img src={confirm_check} alt="confirm_check"/>
-                <h1>75%</h1>
-              </div>
-            </div>
-            <div className='option'>
-              <h1>Actively manage DAO as an investment fund.</h1>
-              <div className='checkbuttons'>
-                <img src={confirm_check} alt="confirm_check"/>
-                <h1>75%</h1>
-              </div>
-            </div>
-          </div>
-          <div className='options'>
-            <div className='option'>
-              <h1>Invest in early stage protocols.</h1>
-              <div className='checkbuttons'>
-                <h1>55%</h1>
-                <img src={confirm_cross} alt="confirm_cross"/>
-              </div>
-            </div>
-            <div className='option'>
-              <h1>Invest in NFT’s.</h1>
-              <div className='checkbuttons'>
-                <h1>55%</h1>
-                <img src={confirm_cross} alt="confirm_cross"/>
-              </div>
-            </div>
+          <div className='social_icon'>
+              <img src={vector_icon} alt="vector_icon"/>
+              <img src={twitter_icon} alt="twitter_icon"/>
+              <img src={telegram_icon} alt="telegram_icon"/>
           </div>
         </div>
-        <div className='buttoncontent'>
+        <div className='content'>
+          <div className='real-content'>
+            <div className='walletinfo'>
+                <div className='walletaddress'>
+                    <img src={connected_status_icon} alt="connected_status_icon"/>
+                    <h1>{walletAddress.substring(0, 4) + "..." + walletAddress.substring(walletAddress.length-2, walletAddress.length)}</h1>
+                </div>
+            </div>
+            <div className='mainlogo'>
+                <img src={mainlogo} alt="mainlogo"/>
+            </div>
+            <div className='title'>
+                <h1>Results</h1>
+            </div>
+            <div className='description'>
+                <h1>Once votes have been collected, the DAO will be open for proposals and use these principles and criteria to evaluate proposals!</h1>
+            </div>
+            <div className='timelabel'>
+              <h1>Genesis ends in:</h1> 
+            </div>
+            <div className='timedialog'>
+              <div className='timedialogdisplay'>
+                <img src={clock_icon} alt="clock_icon"/>
+                <h1>{parseInt(seconds/(24*60*60))}D, {parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))}h, {parseInt((seconds - parseInt(seconds/(24*60*60))*24*60*60 - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))* 60*60)/60)}m, {seconds - parseInt(seconds/(24*60*60))*(24*60*60) - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))*(60*60) - parseInt((seconds - parseInt(seconds/(24*60*60))*24*60*60 - parseInt((seconds - parseInt(seconds/(24*60*60))*(24*60*60))/(60*60))* 60*60)/60)*60}s</h1>
+              </div>
+            </div>
+            <div className='userselect'>
+                <div className='userselectboard'>
+                    <div className='first'>
+                        <h1>1.</h1>
+                        <label></label>
+                    </div>
+                    <div className='second'>
+                        <h1>2.</h1>
+                        <label></label>
+                    </div>
+                    <div className='third'>
+                        <h1>3.</h1>
+                        <label></label>
+                    </div>
+                    <div className='fourth'>
+                        <h1>4.</h1>
+                        <label></label>
+                    </div>
+                    <div className='fifth'>
+                        <h1>5.</h1>
+                        <label></label>
+                    </div>
+                </div>
+            </div>
+            <br></br>
+            <div className='options'>
+              <div className='option'>
+                <h1>Commercialise reputation and verification services.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Provide liquidity to other protocols.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Utilise treasury funds in yield and staking.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Fund bad actor detection and bounty hunting.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Fund development of RDAO ecosystem.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Actively manage DAO as an investment fund.</h1>
+                <div className='checkbuttons'>
+                  <img src={confirm_check} alt="confirm_check"/>
+                  <h1>75%</h1>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Invest in early stage protocols.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+            </div>
+            <div className='options'>
+              <div className='option'>
+                <h1>Invest in NFT’s.</h1>
+                <div className='checkbuttons'>
+                  <h1>55%</h1>
+                  <img src={confirm_cross} alt="confirm_cross"/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='buttoncontent'>
+          </div>
         </div>
       </div>
+      )}
     </div>
   );
 };
